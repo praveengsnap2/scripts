737909d1092eed252e34272690db6aa6c5977686
sachin aggarwal <different.sachin@gmail.com>
2016-06-05 11:48:29 +0530

    create cron

--- Snap2Pay/src/main/resources/Image-analysis-cron-script.sh
+++ Snap2Pay/src/main/resources/Image-analysis-cron-script.sh
@@ -4,37 +4,12 @@
 WS_MACHINE="localhost"
 HOST_ID="AWS1"
 
-#
-#function send_mail ()
-#{
-#FROM="snap2buy@${HOST_ID}.com"
-#TO="different.sachin@gmail.com"
-#REPLYTO="different.sachin@gmail.com"
-#SUBJECT="image processing failed"
-#MAIL_MSG="${HOST_ID}--$1"
-#echo "`date`: msg---$MAIL_MSG"
-#
-#echo "From:$FROM
-#To:$TO
-#Reply-To:$REPLYTO
-#Subject:$SUBJECT
-#
-#$MAIL_MSG
-#
-#This is system generated email, please do not reply.
-#AGI Daemon
-#" | /usr/sbin/sendmail -t different.sachin@gmail.com
-#echo "`date`: Mail sent for-- $1"
-#}
-
 
 echo "checking for already running job"
 
 jobRunning=`ps aux |grep "Image-analysis-cron-script" |grep -v "grep" |wc -l`
 if [ "${jobRunning}" -gt "2" ]; then
        echo "One instance of job is already running "
-       #MSG="One instance of job is already running "
-       #send_mail "$MSG"
        exit 1
 else {
 	    echo "no instance of job is running moving ahead"
@@ -46,8 +21,6 @@ RESULT=`curl http://"${WS_MACHINE}":8080/Snap2Pay-1.0/service/S2P/processNextIma
 if [ $? -ne 0 ]; then
    echo "RESULT=${RESULT}"
    echo "Failed: http://${WS_MACHINE}:8080/Snap2Pay-1.0/service/S2P/processNextImage?"
-   #MSG="Failed: http://${WS_MACHINE}:8080/Snap2Pay-1.0/service/S2P/processNextImage?"
-   #send_mail "$MSG"
    exit 1
 fi
 
